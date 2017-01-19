fc87c11ce9b88aa58d3c24dc6c058cefdd856e8e
Samantha Morco <samanthamorco@gmail.com>
2017-01-18 20:05:54 -0500

    initial commit

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,32 @@
+class GamesController < ApplicationController
+  def name_game_method
+    @name = params[:name].upcase
+    if @name.start_with?("A")
+      @message = "Your name starts with the letter A!"
+    end
+    # @message = "Your name starts with the letter A!"
+    # @image_url = "http://images4.fanpop.com/image/photos/22100000/The-letter-A-the-alphabet-22186936-2560-2560.jpg"
+    # @message2 = ""
+
+    render "name_game_view.html.erb"
+  end
+
+  def guess_number_method
+    @guess = params[:guessed_number].to_i
+    @answer = 42
+    if @guess > @answer
+      @message = "Your guess is too high!"
+    elsif @guess < @answer
+      @message = "Your guess is too low!"
+    else
+      @message = "Your answer is correct!"
+    end
+
+    render "number_game.html.erb"
+  end
+
+  def counter_method
+    @number_of_views = params[:visit].to_i
+    render "counter_view.html.erb"
+  end
+end
